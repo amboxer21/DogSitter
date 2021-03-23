e1178e11c5443ced4546a007658803fe0ced20df
amboxer21 <amboxer21@gmail.com>
2021-03-23 15:02:05 -0400

    Added video file cpy to tmp dir

--- realtime_common_object_detection.py
+++ realtime_common_object_detection.py
@@ -3,12 +3,35 @@ import re
 import cv2
 import time
 import glob
+import shutil
 
 import cvlib as cv
 
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+class FileOpts(object):
+
+    @classmethod
+    def mkdir_p(cls,dir_path):
+        try:
+            os.makedirs(dir_path)
+        except OSError as e:
+            if e.errno == errno.EEXIST and self.dir_exists(dir_path):
+                pass
+            else:
+                raise
+
+    @classmethod
+    def dir_exists(cls,dir_path):
+        return os.path.isdir(dir_path)
+
+    @classmethod
+    def file_exists(cls,file_name):
+        return os.path.isfile(file_name)
+
 class DogSitter(object):
 
-    FPS    = 24.0
+    FPS    = 25.0
     FOURCC = cv2.VideoWriter_fourcc(*'MJPG')
 
     def __init__(self):
@@ -19,7 +42,8 @@ class DogSitter(object):
         self.capture  = None
 
         self.counter  = 0
-        self.vidpath  = '/home/anthony/.dogsitter/videos'
+        self.vidpath  = os.getcwd() + '/videos'
+        self.tmp_dir  = os.getcwd() + '/videos/TMP'
 
     def initialize_video_dot_avi(self, capture, filename):
         self.video_writer(capture, filename)
@@ -57,25 +81,35 @@ class DogSitter(object):
 
     def main(self):
 
+        video_files = {}
+
         bbox, label, conf = str(), str(), str()
 
         capture  = DogSitter.initialize_capture()
         filename = DogSitter.filename_(int(self.img_num() + 1), self.vidpath) 
         output   = DogSitter.initialize_output(filename, capture)
 
+        if not FileOpts.dir_exists(self.tmp_dir):
+            FileOpts.mkdir_p(self.tmp_dir)
+
         while(True):
 
             self.counter += 1
             ret, frame = capture.read()
 
-            if self.counter == 30:
-                self.counter = 0
+            if re.match('(5|10|15|20|25)', str(self.counter)):
+                if self.counter == 25:
+                    self.counter = 0
+
                 bbox, label, conf = cv.detect_common_objects(frame)
         
-                print(label)
-                if 'person' in label and 'bottle' in label:
+                print("[INFO] Label(s): "+str(label))
+                if ('person' and 'bottle') in label:
                     try:
-                        filename = DogSitter.filename_(int(self.img_num() + 1), self.vidpath)
+                        number     = int(self.img_num() + 1)
+                        video_name = "video" + str(number) + ".avi"
+                        filename   = DogSitter.filename_(number, self.vidpath)
+                        video_files[video_name] = filename
 
                         print('[INFO] writing video to '+filename+'.')
 
@@ -86,6 +120,11 @@ class DogSitter(object):
                     except KeyboardInterrupt:
                         capture.release()
                         output.release()
+                elif not ('person' and 'bottle') in label:
+                    # copy list of files into tmp dir, email video then delete
+                    for _file, _path in video_files.items():
+                        shutil.copy(_path, self.tmp_dir+"/"+_file) 
+                    video_files.clear()
 
 if __name__ == '__main__':
     dogsitter = DogSitter()

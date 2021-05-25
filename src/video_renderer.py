e0306b6770192348b70640746e5a20bd3badbfe2
amboxer21 <amboxer21@gmail.com>
2021-05-24 21:32:17 -0400

    finished sorting function.

--- src/video_renderer.py
+++ src/video_renderer.py
@@ -1,3 +1,5 @@
+import re
+import os
 import cv2
 import glob
 
@@ -6,33 +8,45 @@ import numpy as np
 from optparse import OptionParser
 
 class VideoRenderer(object):
+
  
     def __init__(self,config_dict):
 
-        self.size       = ()
-        self.images     = []
+        self.tagged_images_hash = {}
 
         self.path       = config_dict['path']
         self.fps        = int(config_dict['fps'])
         self.glob_regex = config_dict['glob_regex']
         self.fourcc     = cv2.VideoWriter_fourcc(*config_dict['fourcc_ext'])
 
-    def populate_images_array(self):
-        for _file in glob.glob(self.path+self.glob_regex):
-            image = cv2.imread(_file)
-            height, width, layers = image.shape
-            self.size = (width,height)
-            self.images.append(image)
+    @staticmethod
+    def change_directory(path):
+        os.chdir(path)
+
+    def convert_list_into_sorted_hash(self,tagged_images_array=[],image_hash={}):
+        for index in range(0,len(tagged_images_array)):
+            grouped = re.search('(capture)(\d+)(.png)\.([\w\d]+)', tagged_images_array[index], re.I)
+            if not grouped is None:
+                if not grouped.group(4) in image_hash:
+                    image_hash[grouped.group(4)] = []
+                image_hash[grouped.group(4)].append(grouped.group())
+        return image_hash
+
+
+    def images_array(self,tagged_images_hash={}):
+        VideoRenderer.change_directory(self.path)
+        return glob.glob(self.glob_regex+'.*')
  
     def render(self):
+        for key,data in self.convert_list_into_sorted_hash(self.images_array()).items():
+            for f in data:
+                image = cv2.imread(f)
+                height, width, layers = image.shape
+                size = (width,height)
 
-        filename = self.path + 'capture.avi'
-
-        video_writer = cv2.VideoWriter(filename, self.fourcc, self.fps, self.size)
- 
-        for index in range(len(self.images)):
-            video_writer.write(self.images[index])
-            video_writer.release()
+                video_writer = cv2.VideoWriter(key+'.avi', self.fourcc, self.fps, size)
+                video_writer.write(image)
+                video_writer.release()
 
 if __name__ == '__main__':
 
@@ -43,7 +57,7 @@ if __name__ == '__main__':
         help='.')
 
     parser.add_option('-p', '--path',
-        dest='path', default='/home/anthony/Pictures/capture/',
+        dest='path', default='/home/anthony/Pictures/capture/videos/',
         help='.')
 
     parser.add_option('-g', '--glob-regex',
@@ -65,5 +79,4 @@ if __name__ == '__main__':
     }
 
     video_renderer = VideoRenderer(config_dict)
-    video_renderer.populate_images_array()
     video_renderer.render()
